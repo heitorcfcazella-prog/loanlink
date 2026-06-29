3f60769df0cbbd039d997a7cc94369b20f1a13f7
Heitor César <heitorcfcazella@gmail.com>
2026-06-29 15:12:48 -0300

    style: ajuste do tamanho da foto de perfil

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 session_start();
 include("db.php");
 if (isset($_SESSION['id_usuario']) && !empty($_SESSION['foto_perfil'])) {
-  $imagem = '<a href="dashboard.php" style="float: right;"> <img src="' . htmlspecialchars($_SESSION['foto_perfil']) . '" alt="Perfil" style="height:40px; border-radius:50%;"></a>';
+  $imagem = '<a href="dashboard.php" style="float: right;"> <img src="' . htmlspecialchars($_SESSION['foto_perfil']) . '" alt="Perfil" style="height:40px; width:40px; border-radius:50%;"></a>';
 } else {
   $imagem = '<a href="login.php" style="float: right;">Logar</a>';
 }
